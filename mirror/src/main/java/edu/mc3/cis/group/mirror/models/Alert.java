7cf8a2e5da1e596378294769ebe5ca0c8697c638
TheBladesLastKiss <thebladeslastkiss@gmail.com>
2021-12-10 15:04:29 -0500

    Create Alert.java

---
+++ mirror/src/main/java/edu/mc3/cis/group/mirror/models/Alert.java
@@ -0,0 +1,10 @@
+package edu.mc3.cis.group.mirror.models;
+import java.util.*;
+public class Alert{
+    public String sender_name;
+    public String event;
+    public int start;
+    public int end;
+    public String description;
+    public List<String> tags;
+}
